f5575839adadfdc03c1c8d67de20c9148c558403
AnSardG <antoniosardgonzalez@gmail.com>
2023-02-24 20:08:40 +0100

    Correccion ejercicio 3, 4, 5 y utilidades

--- Boletin1º/src/Ej05.java
+++ Boletin1º/src/Ej05.java
@@ -1,35 +1,48 @@
+
+import java.util.GregorianCalendar;
+
 //Programa: Ej05
 //Autor: Antonio Sard González
 public class Ej05 {
-    public static byte leeByte(){
+
+    public static byte leeByte() {
         //Entorno:
         short num;
         //Algoritmo:
-        num=Leer.datoShort();
-        while(num<Byte.MIN_VALUE || num > Byte.MAX_VALUE){
+        num = Leer.datoShort();
+        while (num < Byte.MIN_VALUE || num > Byte.MAX_VALUE) {
             System.out.print("Dato incorrecto. Teclee otro dato: ");
-            num=Leer.datoShort();
+            num = Leer.datoShort();
         }//Fin Mientras
         return (byte) num;
     }
+
     public static void main(String[] args) {
         //Entorno:
-        byte meses, anios;
+        byte meses;
+        String anios;
+        GregorianCalendar fechaHoy, fechaAnios;
+        short auxAnios;
         int dias;
         //Algoritmo:
-        do {
-            System.out.print("Introduzca su edad en a�os: ");
-            anios = Ej05.leeByte();
-            if (anios > 150) {
-                System.out.print("Debe introducir la cantidad de años que tiene,"
-                        + " no el año en el que nació.");
-            }//Fin Si
-        } while (anios < 0 || anios > 150);
-        do {
-            System.out.print("Introduzca los meses: ");
-            meses = Ej05.leeByte();
-        } while (meses < 0 || meses > 12);
-        dias = anios * 360 + meses * 30;
-        System.out.print("Su edad equivale a " + dias + " días.");
+        System.out.print("Introduzca su edad en años: ");
+        anios = Leer.dato();
+        if (anios.length() > 3) {
+            fechaHoy = new GregorianCalendar();
+            fechaAnios = new GregorianCalendar(Integer.valueOf(anios.substring(0, 4))
+                    .intValue() - 1, Integer.valueOf(anios.substring(5, 7)).intValue() + 1,
+                    Integer.valueOf(anios.substring(8)).intValue());
+            auxAnios = (short) (fechaHoy.get(GregorianCalendar.YEAR) - fechaAnios
+                    .get(GregorianCalendar.YEAR));
+            dias = auxAnios * 360 + fechaAnios.get(GregorianCalendar.MONTH) * 30;
+        } else {
+            auxAnios = Short.valueOf(anios).shortValue();
+            do {
+                System.out.print("Introduzca los meses: ");
+                meses = Ej05.leeByte();
+            } while (meses < 0 || meses > 12);
+            dias = auxAnios * 360 + meses * 30;
+        }//Fin Si    
+        System.out.println("Su edad equivale a " + dias + " días.");
     }//Fin Programa
 }
--- Boletin1º/src/Ej06.java
+++ Boletin1º/src/Ej06.java
@@ -1,44 +1,29 @@
-/*
-Programa: Ej06
-Autor: Antonio Sard Gonz·lez
-Entorno:
-	hora es numÈrico entero
-Algoritmo:
-	Repetir
-		Escribir "Introduzca una hora del dÌa (no horas y minutos): "
-		Leer hora
-	Hasta hora >= 0
-	Si hora >=0 y <=24 entonces
-		Si hora <=12 entonces
-			Escribir "Buenos dÌas"
-		Sino
-			Si hora <=18 entonces
-				Escribir "Buenas tardes"
-			Sino
-				Escribir "Buenas noches"
-			Fin Si
-		Fin Si
-	Sino
-		Escribir "Hora fuera de rango"
-	Fin Si
-Fin Programa
-*/
 //Programa: Ej06
-//Autor: Antonio Sard Gonz·lez
+//Autor: Antonio Sard Gonz√°lez
 public class Ej06{
+        public static byte leeByte(){
+            //Entorno:
+            short num;
+            //Algoritmo:
+            num=Leer.datoShort();
+            while(num<Byte.MIN_VALUE || num>Byte.MAX_VALUE){
+                System.out.print("Dato no v√°lido. Teclee otro: ");
+                num=Leer.datoShort();
+            }//Fin Mientras
+            return (byte) num;
+        }//Fin Funci√≥n
 	public static void main(String[] args){
 	//Entorno:
 		byte hora;
-		short auxHora;
 	//Algoritmo:
 		do{
-			System.out.print("Introduzca una hora del dÌa (no horas y minutos): ");
-			auxHora=Leer.datoShort();
-		}while(auxHora < 0);
-		hora=(byte)auxHora;
+			System.out.print("Introduzca una hora del d√≠a(no horas y minutos): ");
+			hora=Ej06.leeByte();
+		}while(hora < 0);
+		hora=(byte)hora;
 		if(hora >=0 && hora <=24){
 			if(hora <=12){
-				System.out.print("Buenos dÌas.");
+				System.out.print("Buenos dÔøΩas.");
 			}else{
 				if(hora <=18){
 					System.out.print("Buenas tardes");
