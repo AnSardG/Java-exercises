06727f47213fbb5a736ec16cc2535e3baf4d8e99
AnSardG <antoniosardgonzalez@gmail.com>
2023-03-08 19:44:04 +0100

    Fixed 2nd exercise

--- Ejercicios Registros CORREGIDO/src/Alumno.java
+++ Ejercicios Registros CORREGIDO/src/Alumno.java
@@ -1,46 +1,52 @@
+
 /**
  *
  * @author Antonio Sard González
  */
 public class Alumno {
     //Atributos:
+
     private String nombre, apellidos;
     private float notaTeoria, notaPractica;
-    private static float notaMedia, aula=2.2F;
-    private static byte numAlumnos;
+    private float aula;
     //Constructores:
-    public Alumno(String nombre, String apellidos, float notaTeoria, 
-            float notaPractica){
-        this.nombre=nombre;
-        this.apellidos=apellidos;
-        this.notaTeoria=notaTeoria;
-        this.notaPractica=notaPractica;
-        Alumno.numAlumnos++;
-        Alumno.notaMedia=(Alumno.notaMedia+this.getNotaTotal());
+
+    public Alumno(String nombre, String apellidos, float notaTeoria,
+            float notaPractica, float aula) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.notaTeoria = notaTeoria;
+        this.notaPractica = notaPractica;
+        this.aula = aula;
+    }
+
+    public Alumno(Alumno alumno) {
+        this.nombre = alumno.nombre;
+        this.apellidos = alumno.apellidos;
+        this.notaTeoria = alumno.notaTeoria;
+        this.notaPractica = alumno.notaPractica;
+        this.aula = alumno.aula;
     }
     //Getters y Setters:
-    public String getNombre(){
+
+    public String getNombre() {
         return this.nombre;
     }
-    public String getApellidos(){
+
+    public String getApellidos() {
         return this.apellidos;
     }
-    public float getNotaTeoria(){
+
+    public float getTeoria() {
         return this.notaTeoria;
     }
-    public float getNotaPractica(){
+
+    public float getPractica() {
         return this.notaPractica;
     }
-    public float getNotaTotal(){
-        return (this.notaPractica+this.notaTeoria)/2;
+
+    public float getAula() {
+        return this.aula;
     }
-    public static float getNotaMedia(){
-        return Alumno.notaMedia/Alumno.numAlumnos;
-    }
-    public static float getAula(){
-        return Alumno.aula;
-    }
-    public static byte getAlumnos(){
-        return Alumno.numAlumnos;
-    }
+
 }
--- Ejercicios Registros CORREGIDO/src/Ej2.java
+++ Ejercicios Registros CORREGIDO/src/Ej2.java
@@ -16,44 +16,61 @@ public class Ej2 {
         return opcion == 's' || opcion == 'S';
     }//Fin Función
 
+    public static Alumno pideAlumno() {
+        //Entorno:
+        String nombre, apellidos;
+        float notaTeoria, notaPractica, aula;
+        //Algoritmo:
+        do {
+            System.out.print("Introduzca el nombre (max. 20 caracteres): ");
+            nombre = Leer.dato();
+        } while (nombre.isEmpty() || nombre.length() > 20);
+        do {
+            System.out.print("Introduzca los apellidos (max. 35 caracteres): ");
+            apellidos = Leer.dato();
+        } while (apellidos.isEmpty() || apellidos.length() > 35);
+        do {
+            System.out.print("Introduzca la nota teórica (0-10): ");
+            notaTeoria = Leer.datoFloat();
+        } while (notaTeoria < 0 || notaTeoria > 10);
+        do {
+            System.out.print("Introduzca la nota práctica (0-10): ");
+            notaPractica = Leer.datoFloat();
+        } while (notaPractica < 0 || notaPractica > 10);
+        System.out.print("Introduzca el aula: ");
+        aula = Leer.datoFloat();
+        return new Alumno(nombre, apellidos, notaTeoria, notaPractica, aula);
+    }
+
     public static void main(String[] args) {
         //Entorno:
         Alumno alumno, alumnoMayorNota;
-        String nombre, apellidos;
-        float notaTeoria, notaPractica, mayorNota;
+        float mayorNota, notaMedia;
+        byte numAlumnos;
         //Algoritmo:
-        mayorNota = -1;
-        alumnoMayorNota = null;
-        do {
-            do {
-                System.out.print("Introduzca el nombre (max. 20 caracteres): ");
-                nombre = Leer.dato();
-            } while (nombre.isEmpty() || nombre.length() > 20);
-            do {
-                System.out.print("Introduzca los apellidos (max. 35 caracteres): ");
-                apellidos = Leer.dato();
-            } while (apellidos.isEmpty() || apellidos.length() > 35);
-            do {
-                System.out.print("Introduzca la nota teórica (0-10): ");
-                notaTeoria = Leer.datoFloat();
-            } while (notaTeoria < 0 || notaTeoria > 10);
-            do {
-                System.out.print("Introduzca la nota práctica (0-10): ");
-                notaPractica = Leer.datoFloat();
-            } while (notaPractica < 0 || notaPractica > 10);
-            alumno = new Alumno(nombre, apellidos, notaTeoria, notaPractica);
-            if (alumno.getNotaTotal() > mayorNota) {
-                alumnoMayorNota = new Alumno(nombre, apellidos, notaTeoria, notaPractica);
-                mayorNota=alumnoMayorNota.getNotaTotal();
+        alumno = Ej2.pideAlumno();
+        alumnoMayorNota = alumno;
+        mayorNota = (alumnoMayorNota.getPractica() + alumnoMayorNota.getTeoria()) / 2;
+        numAlumnos = 1;
+        notaMedia = (alumno.getTeoria() + alumno.getPractica()) / 2;
+        while (deseaContinuar() && numAlumnos <= 30) {
+            alumno = Ej2.pideAlumno();
+            if ((alumno.getTeoria() + alumno.getPractica()) / 2 > mayorNota) {
+                alumnoMayorNota = new Alumno(alumno);
+                mayorNota = (alumnoMayorNota.getPractica()
+                        + alumnoMayorNota.getTeoria()) / 2;
             }//Fin Si
-        } while (deseaContinuar() && Alumno.getAlumnos() <= 30);
+            notaMedia += (alumno.getTeoria() + alumno.getPractica()) / 2;
+            numAlumnos++;
+        }//Fin Mientras
         System.out.println("-----------------------------");
         System.out.println("El alumno con la mayor nota tiene los siguientes datos: ");
         System.out.println("Nombre completo: " + alumnoMayorNota.getNombre() + " "
                 + alumnoMayorNota.getApellidos());
-        System.out.println("Nota: " + alumnoMayorNota.getNotaTotal());
-        System.out.println("Aula: " + Alumno.getAula());
+        System.out.println("Nota: " + (alumnoMayorNota.getPractica()
+                + alumnoMayorNota.getTeoria()) / 2);
+        System.out.println("Aula: " + alumnoMayorNota.getAula());
         System.out.println("-----------------------------");
-        System.out.println("La nota media del grupo es: " + Alumno.getNotaMedia());
+        System.out.println("La nota media del grupo es: " + notaMedia / numAlumnos);
     }//Fin Programa
 }
