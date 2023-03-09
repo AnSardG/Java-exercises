45c1b9e4a83b43f37d245d70b8e9ffe805f15000
AnSardG <antoniosardgonzalez@gmail.com>
2023-03-09 15:47:08 +0100

    Fixed exercise 8

--- Ejercicios Vectores/src/Ej8.java
+++ Ejercicios Vectores/src/Ej8.java
@@ -5,36 +5,48 @@
  */
 public class Ej8 {
 
-    public static void main(String[] args) {
+    public static byte preguntaNotas() {
         //Entorno:
-        byte[] calificacion;
-        byte cantNotas, nota, i, frecuencia;
+        byte cantidad;
         String op;
         //Algoritmo:
         do {
             System.out.print("¿Sabe cuántas notas va a introducir? (s/n): ");
             op = Leer.dato();
-        } while ("sSnN".indexOf(op) > 0 || op.isEmpty());
+        } while ("sSnN".indexOf(op) < 0 || op.isEmpty());
         if ("s".equals(op.toLowerCase())) {
             do {
                 System.out.print("¿Cuántas notas quiere introducir?: ");
-                cantNotas = Utilidades.leeByte();
-                if(cantNotas){
-                    
-                }
-            } while (cantNotas < 1);
+                cantidad = Utilidades.leeByte();
+            } while (cantidad < 1);
         } else {
-            cantNotas = 30;
+            cantidad = 30;
         }//Fin Si
-        calificacion = new byte[cantNotas];
-        nota = 0;
+        return cantidad;
+    }//Fin Función
+
+    public static void main(String[] args) {
+        //Entorno:
+        byte[] calificacion;
+        byte cantNotas, numNotas, i, nota;
+        //Algoritmo:
+        cantNotas = Ej8.preguntaNotas();
+        calificacion = new byte[11];
+        numNotas = 0;
         do {
-            System.out.print("Introduzca la nota " + (nota + 1) + ": ");
-            calificacion[nota] = Utilidades.leeByte();
-            nota++;
-        } while (nota < cantNotas && calificacion[nota - 1] >= 0
-                && calificacion[nota - 1] <= 10);
+            System.out.print("Introduzca la nota " + (numNotas + 1) + ": ");
+            nota = Utilidades.leeByte();
+            if (nota >= 0 && nota < calificacion.length - 1) {
+                calificacion[nota] += 1;
+            }//Fin Si
+            numNotas++;
+        } while (numNotas < cantNotas && nota >= 0 && nota <= 10);
         System.out.println("Lista de frecuencias de cada una de las notas: ");
-        
+        for (i = 0; i <= calificacion.length - 1; i++) {
+            if (calificacion[i] != 0) {
+                System.out.println("Nota: " + i + ". Frecuencia: " + calificacion[i]
+                        + ".");
+            }//Fin Si
+        }//Fin Para
     }//Fin Programa
 }
