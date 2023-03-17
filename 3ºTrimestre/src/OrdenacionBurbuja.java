c855c5dd5e80ebe3c81836b133e4605ed57aaa16
AnSardG <antoniosardgonzalez@gmail.com>
2023-03-17 12:05:28 +0100

    Fix array exercise

--- 3ºTrimestre/src/OrdenacionBurbuja.java
+++ 3ºTrimestre/src/OrdenacionBurbuja.java
@@ -17,20 +17,25 @@ public class OrdenacionBurbuja {
     public static void main(String[] args) {
         //Entorno:
         byte[] vector = {22, 13, 17, 12, 7, 15, 2};
-        byte num, i, pos;
+        byte num, pos, fin;
+        boolean desordenado;
         //Algoritmo:
         System.out.println("Tabla sin ordenar: ");
         OrdenacionBurbuja.muestraTabla(vector);
         System.out.println("\b\b.\n-------------------------------");
-        for (i = 0; i <= vector.length - 1; i++) {
-            for (pos = 0; pos <= vector.length - 1; pos++) {
-                if (pos + 1 <= vector.length - 1 && vector[pos] > vector[pos + 1]) {
+        fin = (byte)(vector.length - 1);
+        do{
+            desordenado = false;
+            for (pos = 0; pos <= fin; pos++) {
+                if (pos + 1 <= fin && vector[pos] > vector[pos + 1]) {
                     num = vector[pos + 1];
                     vector[pos + 1] = vector[pos];
                     vector[pos] = num;
+                    desordenado = true;
                 }//Fin Si
             }//Fin Para
-        }//Fin Para
+            fin--;
+        }while(desordenado) ;
         System.out.println("Tabla ordenada: ");
         OrdenacionBurbuja.muestraTabla(vector);
         System.out.println("\b\b.\n-------------------------------");
