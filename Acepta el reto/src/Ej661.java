e88280ec1f1c64fa7936dbb77178bb9992368596
AnSardG <antoniosardgonzalez@gmail.com>
2023-03-21 18:59:10 +0100

    Try exercise 661

--- Acepta el reto/src/Ej661.java
+++ Acepta el reto/src/Ej661.java
@@ -8,25 +8,22 @@ import java.io.InputStreamReader;
  * @author Antonio Sard González
  */
 public class Ej661 {
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader teclado;
-        String linea, obj;
+        String linea, obj, salida;
         String[] objetos;
         int[] valores, pesos;
         int i, valor, peso, pos, posAux;
-        
+
         teclado = new BufferedReader(new InputStreamReader(System.in));
-        linea = "";
-        while (!"0".equals(linea)) {
-            linea = teclado.readLine();
-            pos = Integer.valueOf(linea);
+        linea = teclado.readLine();
+        pos = Integer.valueOf(linea);
+        while (pos != 0) {
             objetos = new String[pos];
             valores = new int[pos];
             pesos = new int[pos];
-            do {
-                linea = teclado.readLine();
-            } while (linea.matches("^[a-z]{1,9} \\d \\d$"));
+            linea = teclado.readLine();
             obj = linea.substring(0, linea.indexOf(" "));
             linea = linea.substring(linea.indexOf(" ") + 1);
             valor = Integer.valueOf(linea.substring(0, linea.indexOf(" ")));
@@ -41,13 +38,13 @@ public class Ej661 {
                 valor = Integer.valueOf(linea.substring(0, linea.indexOf(" ")));
                 peso = Integer.valueOf(linea.substring(linea.indexOf(" ") + 1));
                 pos = 0;
-                while (pos <= i && valor < valores[pos]) {
+                while (pos <= i && (valor < valores[pos] || (valor == valores[pos]
+                        && peso >= pesos[pos]))) {
                     pos++;
                 }
-                while (valor == valores[pos] && pos <= i && peso < pesos[pos]) {
+                while (pos > 0 && valor == valores[pos] && peso < pesos[pos - 1]) {
                     pos--;
                 }
-                
                 for (posAux = i; posAux >= pos; posAux--) {
                     if (posAux > 0) {
                         objetos[posAux] = objetos[posAux - 1];
@@ -59,11 +56,13 @@ public class Ej661 {
                 valores[pos] = valor;
                 pesos[pos] = peso;
             }
-            linea = "";
+            salida = "";
             for (i = 0; i < objetos.length; i++) {
-                linea += objetos[i] + " ";
+                salida += objetos[i] + " ";
             }
-            System.out.println(linea.trim());
+            System.out.println(salida.trim());
+            linea = teclado.readLine();
+            pos = Integer.valueOf(linea);
         }
     }
 }
