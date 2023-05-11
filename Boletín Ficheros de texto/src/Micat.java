973e9d5633ebf34e26d2b31a376fa24ffc652bb5
AnSardG <antoniosardgonzalez@gmail.com>
2023-05-11 13:19:37 +0200

    Done 1st directoy exercises

---
+++ Boletín Ficheros de texto/src/Micat.java
@@ -0,0 +1,47 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author Antonio Sard González
+ */
+public class Micat {
+
+    public static void main(String[] args) {
+        //Entorno:
+        BufferedReader fSalida;
+        String linea;
+        int i;
+        //Algoritmo:
+        fSalida = null;
+        if (args.length > 0) {
+            for (String file: args) {
+                try {
+                    fSalida = new BufferedReader(new FileReader(file));
+                    linea = fSalida.readLine();
+                    while (linea != null) {
+                        System.out.println(linea);
+                        linea = fSalida.readLine();
+                    }//Fin Mientras         
+                } catch (FileNotFoundException fnfe) {
+                    System.out.println("No existe " + file + ".");
+                } catch (IOException ioe) {
+                    System.out.println("Error de E/S al leer el archivo " + file + ".");
+                } finally {
+                    if (fSalida != null) {
+                        try {
+                            fSalida.close();
+                        } catch (IOException ioe) {
+                            System.out.println("Error de E/S al leer el archivo " + file + ".");
+                        }//Fin Try                 
+                    }//Fin Si
+                }//Fin Try
+            }//Fin Foreach
+        } else {
+            System.out.println("Número de argumentos inválido, introduzca el nombre de un archivo.");
+        }//Fin Si         
+    }//Fin Programa
+}
