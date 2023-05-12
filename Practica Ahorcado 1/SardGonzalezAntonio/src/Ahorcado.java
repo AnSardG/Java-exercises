70658bd78056eab574361bfdcdf85337599c6a0b
Antonio Sard González <antoniosardgonzalez@gmail.com>
2023-05-12 11:02:24 +0200

    Finish 1st half of the last practice + fix diagram

--- Practica Ahorcado 1/SardGonzalezAntonio/src/Ahorcado.java
+++ Practica Ahorcado 1/SardGonzalezAntonio/src/Ahorcado.java
@@ -3,14 +3,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 /**
  *
  * @author Antonio Sard González
  */
 public class Ahorcado {
 
-    public static byte pideDificultad(){
+    public static byte pideDificultad() {
         //Entorno:
         BufferedReader teclado;
         String dificultad;
@@ -20,24 +19,83 @@ public class Ahorcado {
         System.out.print("Elige opción: ");
         teclado = new BufferedReader(new InputStreamReader(System.in));
         dificultad = "-1";
-        try{            
+        try {
             dificultad = teclado.readLine();
-            while((Long.valueOf(dificultad) < Byte.MIN_VALUE || Long.valueOf(dificultad) > Byte.MAX_VALUE) 
-                    || (Byte.valueOf(dificultad) != 1 && Byte.valueOf(dificultad) != 2)){
+            while ((Long.valueOf(dificultad) < Byte.MIN_VALUE || Long.valueOf(dificultad) > Byte.MAX_VALUE)
+                    || (Byte.valueOf(dificultad) != 1 && Byte.valueOf(dificultad) != 2)) {
                 System.out.print("Elige una opción correcta [1-2]: ");
                 dificultad = teclado.readLine();
             }//Fin Mientras
-        } catch (IOException ioe){
-            System.out.println("Error de E/S.");        
+        } catch (IOException ioe) {
+            System.out.println("Error de E/S.");
         }//Fin Try       
         return Byte.valueOf(dificultad);
     }//Fin Función
-    
+
+    public static String pidePalabra() {
+        //Entorno:
+        BufferedReader teclado;
+        String palabra;
+        //Algoritmo:
+        teclado = new BufferedReader(new InputStreamReader(System.in));
+        palabra = "";
+        try {
+            palabra = teclado.readLine();
+            while (palabra.isEmpty() || !palabra.matches("[a-zA-ZñÑÁÉÓÍÚáéóíú]+")) {
+                System.out.print("Palabra incorrecta, introduzca una palabra: ");
+                palabra = teclado.readLine();
+            }//Fin Mientras
+        } catch (IOException ioe) {
+            System.out.println("Error de E/S.");
+        }//Fin Try
+        return palabra;
+    }//Fin Función
+
+    public static char pideLetra() {
+        //Entorno:
+        BufferedReader teclado;
+        String linea;
+        char letra;
+        //Algoritmo:
+        letra = '\n';
+        teclado = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            linea = teclado.readLine();
+            while (linea.isEmpty() || !linea.matches("[a-zA-ZñÑÁÉÓÍÚáéóíú]")) {
+                System.out.print("Letra incorrecta. Introduzca una letra correcta: ");
+                linea = teclado.readLine();
+            }//Fin Mientras
+            letra = linea.charAt(0);
+        } catch (IOException ioe) {
+            System.out.println("Error de E/S.");
+        }//Fin Try
+        return letra;
+    }//Fin Función
+
     public static void main(String[] args) {
         //Entorno:
-        
+        AhorcadoDificil aDificil;
+        AhorcadoFacil aFacil;
+        byte res;
         //Algoritmo:
-        System.out.println(Ahorcado.pideDificultad());
+        if (Ahorcado.pideDificultad() == 1) {
+            System.out.print("Introduzca la palabra secreta: ");
+            aFacil = new AhorcadoFacil(Ahorcado.pidePalabra());
+            do {
+                System.out.print("Introduzca una letra: ");
+                aFacil.compruebaLetra(Ahorcado.pideLetra());
+                res = aFacil.muestraResultado();
+                aFacil.muestraListaFallos();
+            } while (res == -1 && aFacil.getFallos() < aFacil.FALLOSPERMITIDOS);
+        } else {
+            System.out.print("Introduzca la palabra secreta: ");
+            aDificil = new AhorcadoDificil(Ahorcado.pidePalabra());
+            do {
+                System.out.print("Introduzca una letra: ");
+                aDificil.compruebaLetra(Ahorcado.pideLetra());
+                res = aDificil.muestraResultado();
+            } while (res == -1 && aDificil.getFallos()
+                    < AhorcadoDificil.FALLOSPERMITIDOS);
+        }//Fin Si              
     }//Fin Programa
-
 }
---
+++ Practica Ahorcado 1/SardGonzalezAntonio/src/AhorcadoFacil.java
@@ -0,0 +1,51 @@
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Antonio Sard González
+ */
+public class AhorcadoFacil extends AhorcadoDificil {
+    //Atributos:
+
+    private LinkedList<Character> listaFallos;
+    //Constructores:
+
+    public AhorcadoFacil(String palabraSecreta) {
+        super(palabraSecreta);
+        listaFallos = new LinkedList();
+    }
+    //Métodos:
+
+    @Override
+    public void compruebaLetra(char letra) {
+        //Entorno:
+        int cantLetra;
+        //Algoritmo:
+        letra = Character.toLowerCase(letra);
+        cantLetra = palabraSecreta.length()
+                - palabraSecreta.toLowerCase().replaceAll(String.valueOf(letra), "").length();
+        if (cantLetra > 0) {
+            this.modificaPalabraRespuesta(letra, cantLetra);
+        } else if (listaFallos.indexOf(letra) < 0) {
+            listaFallos.add(letra);
+            this.incrementaFallos();
+            this.modificaDibujo();
+        }//Fin Si             
+    }
+
+    public void muestraListaFallos() {
+        //Entorno:
+        Iterator<Character> iterador;
+        String linea;
+        //Algoritmo:
+        iterador = listaFallos.iterator();
+        linea = "Letras fallidas: ";
+        while (iterador.hasNext()) {
+            linea += iterador.next() + ", ";
+        }//Fin Mientras
+        linea = linea.substring(0, linea.length() - 2) + ".";
+        System.out.println(linea);
+    }
+}
