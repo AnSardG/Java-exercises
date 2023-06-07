d242b8969adc692820562be6d495c27d8fa3ba25
Antonio Sard González <antoniosardgonzalez@gmail.com>
2023-06-07 23:46:24 +0200

    Fix practice 3

--- Practicas corregidas/Practica3/src/Practica3.java
+++ Practicas corregidas/Practica3/src/Practica3.java
@@ -38,23 +38,17 @@ public class Practica3 {
         int num;
         //Algoritmo:
         numAleatorio = new Random();
-        do {
-            num = numAleatorio.nextInt(1000001);
-        } while (num == 0);
+        num = numAleatorio.nextInt(1000000) + 1;
         return num;
     }//Fin Función
 
     public static void muestraResultado(int numAleatorio, int numIntroducido) {
         //Entorno:
-        int auxAleatorio, auxIntroducido;
-        byte cifraNum;
+        byte cifraNum, diferencia;
         //Algoritmo:
-        auxAleatorio = numAleatorio;
-        auxIntroducido = numIntroducido;
         if (cifras(numAleatorio) > cifras(numIntroducido)) {
-            while (cifras(auxAleatorio) > cifras(auxIntroducido)) {
+            for(diferencia = (byte)(cifras(numAleatorio) - cifras(numIntroducido)); diferencia >= 1; diferencia--) {
                 System.out.print("*");
-                auxAleatorio = auxAleatorio / 10;
             }//Fin Mientras
             numAleatorio = numAleatorio - (int) (numAleatorio
                     / Math.pow(10, cifras(numIntroducido)))
@@ -79,32 +73,29 @@ public class Practica3 {
         //Entorno:
         int numAleatorio, numIntroducido;
         byte oportunidad, cifrasAleatorio;
-        boolean incorrecto;
         //Algoritmo:
         numAleatorio = numeroAleatorio();
-        incorrecto = true;
         oportunidad = 1;
         System.out.print("Tiene 10 oportunidades para adivinar el siguiente número: ");
         for (cifrasAleatorio = cifras(numAleatorio); cifrasAleatorio >= 1; cifrasAleatorio--) {
             System.out.print("*");
         }//Fin Para
         System.out.println(" (" + cifras(numAleatorio) + " dígitos)");
-        do {
-            System.out.print("Introduzca un número (" + oportunidad 
+        System.out.print("Introduzca un número (" + oportunidad
                     + "ª oportunidad): ");
             numIntroducido = Leer.datoInt();
-            if (numAleatorio != numIntroducido) {
-                muestraResultado(numAleatorio, numIntroducido);
-                oportunidad++;
-            } else {
-                incorrecto = false;
-            }//Fin Si
-        } while (incorrecto && oportunidad <= 10);
-        if (incorrecto) {
-            System.out.println("Ha perdido, el número correcto es el " 
+        while(numAleatorio != numIntroducido && oportunidad < 10) {            
+            muestraResultado(numAleatorio, numIntroducido);
+            oportunidad++;
+            System.out.print("Introduzca un número (" + oportunidad
+                    + "ª oportunidad): ");
+            numIntroducido = Leer.datoInt();
+        }//Fin Mientras
+        if (numAleatorio != numIntroducido) {
+            System.out.println("Ha perdido, el número correcto es el "
                     + numAleatorio + ".");
         } else {
-            System.out.println("¡Ha acertado, el número correcto es el " + numAleatorio 
+            System.out.println("¡Ha acertado, el número correcto es el " + numAleatorio
                     + ", ha ganado en la " + oportunidad + "ª oportunidad!");
         }//Fin Si
     }//Fin Programa
